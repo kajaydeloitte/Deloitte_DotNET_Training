2916437128051484ec03e6336b31493e3261e96a
kajaydeloitte <ajaykonda1304@gmail.com>
2024-09-03 17:52:30 +0530

    Create One.cs

---
+++ Day 2/One.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string name, id, job, salary;
+ id = Console.ReadLine();
+ name = Console.ReadLine();
+ job = Console.ReadLine();
+ salary = Console.ReadLine();
+
+ Console.WriteLine("ID : {0}, Name : {1}, Job : {2}, Salary : {3}",id,name,job,salary);
+
+
+      
+        }
+    }
+}
