805a0c7d03f01e8954da388972dad6744d4fa9dd
kajaydeloitte <ajaykonda1304@gmail.com>
2024-09-10 16:40:43 +0530

    Add files via upload

---
+++ Day 2/WebApplication6/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿//namespace WebApplication6.Models
+//{
+//    public class Product
+//    {
+//        public int ProductId { get; set; }
+//        public string ProductName { get; set; }
+//        public int Quantity { get; set; }
+//        public double UnitPrice { get; set; }
+//    }
+//}
